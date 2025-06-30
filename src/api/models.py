1f6dcb639052596ed0d1f8ce546dcb8b540a4ded
Shane B. <dotfortun@users.noreply.github.com>
2025-06-30 23:01:15 +0000

    Added a book model.

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,83 @@
+from datetime import date
+
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import (
+    String, Column, Table, ForeignKey
+)
+from sqlalchemy.orm import (
+    DeclarativeBase, Mapped,
+    mapped_column, relationship,
+)
 
-db = SQLAlchemy()
 
-class User(db.Model):
+class Base(DeclarativeBase):
+    """
+    This is magic that can be ignored
+    for now!  It's a special tool
+    that will help us later.
+    """
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+"""
+This is the old 1.4-style models:
+"""
+# class User(db.Model):
+#     __tablename__ = "user"
+#     id = db.Column(
+#         db.Integer,
+#         primary_key=True,
+#     )
+#     username = db.Column(
+#         db.String(256),
+#         unique=True,
+#         nullable=False,
+#     )
+#     email = db.Column(
+#         db.String(256),
+#     )
+#     password = db.Column(
+#         db.String(256),
+#         nullable=False,
+#     )
+
+
+class User(Base):
+    """
+    This is the new SQA 2.0 style:
+    """
+    __tablename__ = "user"
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    username: Mapped[str] = mapped_column(
+        String(256), unique=True, nullable=False,
+    )
+    email: Mapped[str]
+    password: Mapped[str] = mapped_column(
+        String(256), nullable=False,
+    )
 
 
-    def serialize(self):
+class Book(Base):
+    __tablename__ = "book"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    author: Mapped[str]
+    num_pages: Mapped[int]
+    date_published: Mapped[date]
+    cover: Mapped[str]
+    is_awesome: Mapped[bool]
+
+    def serialize(self) -> dict:
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "title": self.title,
+            "author": self.author,
+            "num_pages": self.num_pages,
+            "date_published": self.date_published,
+            "cover": self.cover,
+            "is_awesome": self.is_awesome,
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,8 +1,9 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+from typing import List
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Book
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -13,56 +14,56 @@ api = Blueprint('api', __name__)
 # Allow CORS requests to this API
 CORS(api)
 
-books = [
-    {
-        "num_pages": 293,
-        "year_published": 1997,
-        "author": "Ray Bradbury",
-        "isbn10": "0-380-72940-7",
-        "have_read": True,
-        "cover": "https://pictures.abebooks.com/isbn/9780553136951-us.jpg",
-        "title": "Something Wicked This Way Comes",
-        "isbn13": "978-0-380-72940-1",
-        "is_awesome": True,
-        "id": 2,
-    },
-    {
-        "num_pages": 383,
-        "year_published": 1970,
-        "author": "Gabriel Garcia Marquez",
-        "isbn10": "0-380-01503-X",
-        "have_read": True,
-        "cover": "https://m.media-amazon.com/images/I/81dy4cfPGuL._SY522_.jpg",
-        "title": "One Hundred Years of Solitude",
-        "isbn13": None,
-        "is_awesome": True,
-        "id": 3,
-    },
-    {
-        "num_pages": 470,
-        "year_published": 1992,
-        "author": "Neal Stephenson",
-        "isbn10": None,
-        "have_read": False,
-        "cover": "https://i5.walmartimages.com/seo/Snow-Crash-Hardcover-9780613361620_f11eea3c-5e60-4a1b-936b-67c9c4455e27_1.0a9c061d4600a35fb739ad85e9e9aa06.jpeg",
-        "title": "Snow Crash",
-        "isbn13": "978-061336162",
-        "is_awesome": True,
-        "id": 4,
-    },
-    {
-        "num_pages": 815,
-        "year_published": 2002,
-        "author": "Douglas Adams",
-        "isbn10": None,
-        "have_read": False,
-        "cover": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1404613595i/13.jpg",
-        "title": "The Ultimate Hitchiker's Guide To The Galaxy",
-        "isbn13": "978-0-645-45374-7",
-        "is_awesome": True,
-        "id": 5,
-    },
-]
+# books = [
+#     {
+#         "num_pages": 293,
+#         "year_published": 1997,
+#         "author": "Ray Bradbury",
+#         "isbn10": "0-380-72940-7",
+#         "have_read": True,
+#         "cover": "https://pictures.abebooks.com/isbn/9780553136951-us.jpg",
+#         "title": "Something Wicked This Way Comes",
+#         "isbn13": "978-0-380-72940-1",
+#         "is_awesome": True,
+#         "id": 2,
+#     },
+#     {
+#         "num_pages": 383,
+#         "year_published": 1970,
+#         "author": "Gabriel Garcia Marquez",
+#         "isbn10": "0-380-01503-X",
+#         "have_read": True,
+#         "cover": "https://m.media-amazon.com/images/I/81dy4cfPGuL._SY522_.jpg",
+#         "title": "One Hundred Years of Solitude",
+#         "isbn13": None,
+#         "is_awesome": True,
+#         "id": 3,
+#     },
+#     {
+#         "num_pages": 470,
+#         "year_published": 1992,
+#         "author": "Neal Stephenson",
+#         "isbn10": None,
+#         "have_read": False,
+#         "cover": "https://i5.walmartimages.com/seo/Snow-Crash-Hardcover-9780613361620_f11eea3c-5e60-4a1b-936b-67c9c4455e27_1.0a9c061d4600a35fb739ad85e9e9aa06.jpeg",
+#         "title": "Snow Crash",
+#         "isbn13": "978-061336162",
+#         "is_awesome": True,
+#         "id": 4,
+#     },
+#     {
+#         "num_pages": 815,
+#         "year_published": 2002,
+#         "author": "Douglas Adams",
+#         "isbn10": None,
+#         "have_read": False,
+#         "cover": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1404613595i/13.jpg",
+#         "title": "The Ultimate Hitchiker's Guide To The Galaxy",
+#         "isbn13": "978-0-645-45374-7",
+#         "is_awesome": True,
+#         "id": 5,
+#     },
+# ]
 
 
 @api.route("/library", methods=["POST"])
@@ -77,9 +78,13 @@ def create_book():
 
 @api.route("/library", methods=["GET"])
 def read_books() -> tuple[str, int]:
+    books: List[Book] = db.session.scalars(
+        db.select(Book)
+    ).all()
+
     return jsonify(
-        books=books, total=len(books),
-        offset=0, count=len(books)
+        books=[book.serialize() for book in books],
+        total=len(books), offset=0, count=len(books)
     ), 200
 
 
