bcbfa00b9e0c631690ac19ac08a157781abe6644
Shane B. <dotfortun@users.noreply.github.com>
2025-07-02 23:06:31 +0000

    Talked About Relationships

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,8 +1,27 @@
 
 import os
 from flask_admin import Admin
-from .models import db, User, Book
+from .models import db, User, Book, Author, Genre
 from flask_admin.contrib.sqla import ModelView
+
+
+class BookView(ModelView):
+    column_list = [
+        'id', 'title', 'author', 'num_pages',
+        'date_published', 'cover', 'is_awesome'
+    ]
+
+
+class AuthorView(ModelView):
+    column_list = [
+        'id', 'name', 'dob', 'gender', 'books'
+    ]
+
+
+class GenreView(ModelView):
+    column_list = [
+        'id', 'name', 'books'
+    ]
 
 
 def setup_admin(app):
@@ -12,7 +31,9 @@ def setup_admin(app):
 
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Book, db.session))
+    admin.add_view(AuthorView(Author, db.session))
+    admin.add_view(BookView(Book, db.session))
+    admin.add_view(GenreView(Genre, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -1,8 +1,9 @@
 from datetime import date
+from typing import List
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import (
-    String, Column, Table, ForeignKey
+    String, Column, Table, ForeignKey,
 )
 from sqlalchemy.orm import (
     DeclarativeBase, Mapped,
@@ -44,6 +45,22 @@ This is the old 1.4-style models:
 #     )
 
 
+book_to_genre = Table(
+    "book_to_genre",
+    Base.metadata,
+    Column("book_id", ForeignKey("book.id")),
+    Column("genre_id", ForeignKey("genre.id")),
+)
+
+
+# class BookToGenre(Base):
+#     __tablename__ = "book_to_genre_2"
+
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     book_id: Mapped[int] = mapped_column(ForeignKey("book.id"))
+#     genre_id: Mapped[int] = mapped_column(ForeignKey("genre.id"))
+
+
 class User(Base):
     """
     This is the new SQA 2.0 style:
@@ -65,19 +82,62 @@ class Book(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str]
-    author: Mapped[str]
+    author_id: Mapped[int] = mapped_column(ForeignKey("author.id"))
+    author: Mapped["Author"] = relationship(back_populates="books")
     num_pages: Mapped[int]
     date_published: Mapped[date]
     cover: Mapped[str]
     is_awesome: Mapped[bool]
+    genres: Mapped[List["Genre"]] = relationship(
+        back_populates="books",
+        secondary=book_to_genre,
+    )
 
     def serialize(self) -> dict:
         return {
             "id": self.id,
             "title": self.title,
-            "author": self.author,
+            # "author": self.author,
             "num_pages": self.num_pages,
             "date_published": self.date_published,
             "cover": self.cover,
             "is_awesome": self.is_awesome,
         }
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
+
+
+class Author(Base):
+    __tablename__ = "author"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    dob: Mapped[date]
+    gender: Mapped[str]
+    books: Mapped[List["Book"]] = relationship(back_populates="author")
+
+    def serialize(self) -> dict:
+        return {
+            "id": self.id,
+            "name": self.name,
+            "dob": self.dob,
+            "gender": self.gender,
+        }
+
+    def __repr__(self):
+        return f"<Author {self.name}>"
+
+
+class Genre(Base):
+    __tablename__ = "genre"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    books: Mapped[List["Book"]] = relationship(
+        back_populates="genres",
+        secondary=book_to_genre,
+    )
+
+    def __repr__(self):
+        return f"<Genre {self.name}>"
